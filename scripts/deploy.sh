7e6cce9296e00c554697889524f4caf547ee8577
fiyosa <fiyosa97@gmail.com>
2026-06-18 14:16:39 +0700

    feat: deploy/be-manual_fe-manual

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,6 @@
 set -e
 
 echo "Deploying..."
-source /root/docker/go-fiber-vue/.env
-docker build \
-  --build-arg VITE_API_URL="$VITE_API_URL" \
-  -t portfolio \
-  -f build/package/Dockerfile .
 docker stop portfolio 2>/dev/null || true
 docker rm portfolio 2>/dev/null || true
 
